73f6d2511e94722ad10f6302538fbfa49927d656
mdmridul6 <mdmridul608@hotmail.com>
2022-02-03 22:32:17 +0600

    debuger remove

--- config/app.php
+++ config/app.php
@@ -166,7 +166,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -231,7 +230,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
 
     ],
 
