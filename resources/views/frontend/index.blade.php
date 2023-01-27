9d4a3fb9573f8f3c0dc3a2539fd83ad5b99aaf19
mdmridul608 <mdmridul608@gmail.com>
2023-01-27 21:02:31 +0600

    dsa

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -260,44 +260,47 @@
     </section>
     <!-- END TEAM -->
 
-    <!-- START PRICING TABLE -->
-    <section id="price" class="section_padding">
-        <div class="container">
-            <div class="section_heading wow zoomIn text-center">
-                <h2>our <span>pricing</span></h2>
 
-            </div> <!-- END HEADING -->
+    @if (isset($data['package']) && $data['package']->count() > 0)
+        <!-- START PRICING TABLE -->
+        <section id="price" class="section_padding">
+            <div class="container">
+                <div class="section_heading wow zoomIn text-center">
+                    <h2>our <span>pricing</span></h2>
 
-            <div class="row">
-                @foreach ($data['package'] as $package)
-                    <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.1s"
-                        data-wow-offset="0">
-                        <div class="pricingTable">
-                            <div class="pricingTable-header">
-                                <div class="heading">
-                                    <h3>{{ $package->name }}</h3>
-                                    <span class="subtitle">{{ $package->sort_desc }}</span>
-                                </div>
-                                <span class="price-value">৳<span>{{ $package->price }}</span><span
-                                        class="mo">/month</span></span>
-                            </div> <!-- END PRICING HEADER -->
+                </div> <!-- END HEADING -->
 
-                            <div class="pricingContent">
-                                <ul>
-                                    @for ($i = 0; $i < count($package->benifit); $i++)
-                                        <li class="text-capitalize">{{ $package->benifit[$i] }}</li>
-                                    @endfor
+                <div class="row">
+                    @foreach ($data['package'] as $package)
+                        <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInUp" data-wow-duration="1s"
+                            data-wow-delay="0.1s" data-wow-offset="0">
+                            <div class="pricingTable">
+                                <div class="pricingTable-header">
+                                    <div class="heading">
+                                        <h3>{{ $package->name }}</h3>
+                                        <span class="subtitle">{{ $package->sort_desc }}</span>
+                                    </div>
+                                    <span class="price-value">৳<span>{{ $package->price }}</span><span
+                                            class="mo">/month</span></span>
+                                </div> <!-- END PRICING HEADER -->
 
-                                </ul>
-                            </div> <!-- END PRICING CONTENT -->
+                                <div class="pricingContent">
+                                    <ul>
+                                        @for ($i = 0; $i < count($package->benifit); $i++)
+                                            <li class="text-capitalize">{{ $package->benifit[$i] }}</li>
+                                        @endfor
 
-                        </div> <!-- END PRICING TABLE -->
-                    </div> <!-- END COL -->
-                @endforeach
+                                    </ul>
+                                </div> <!-- END PRICING CONTENT -->
+
+                            </div> <!-- END PRICING TABLE -->
+                        </div> <!-- END COL -->
+                    @endforeach
+                </div>
             </div>
-        </div>
-    </section>
-    <!-- END PRICING TABLE -->
+        </section>
+        <!-- END PRICING TABLE -->
+    @endif
 
 
     <!-- START CONTACT -->
